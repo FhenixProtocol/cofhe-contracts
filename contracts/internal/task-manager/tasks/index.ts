3fb7770fb6ffe97f2ed847a70c8b7ba34f86d636
liorbond <liorbond@gmail.com>
2025-12-17 21:46:49 +0200

    remove

--- contracts/internal/task-manager/tasks/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./doAdd";
-export * from "./doAddNonTrivial";
-export * from "./doDecrypt";
-export * from "./deployContract";
-export * from "./doRandom";
-export * from "./upgradeTM";
-export * from "./deployExample";
